6a41de39e45441f2fef154a51ded9cba6aa2bc07
Alexander João Peterson Santos <git@alexjps.com>
2026-01-05 13:47:59 +0000

    feat(backend): ReDoc and Swagger for backend API
    Redoc is accessible via /api/schema/redoc
    Swagger is accessible via /api/schema/swagger
    A YAML file for the API is available at /api/schema

--- backend/beifahrer_backend/urls.py
+++ backend/beifahrer_backend/urls.py
@@ -1,9 +1,25 @@
 import chat.views as views
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/chat/<str:chat_id>", views.chat_view, name="chat"),
     path("api/chat/", views.post_new_chat, name="chat-new"),
 ]
